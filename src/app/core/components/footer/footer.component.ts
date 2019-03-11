cf02d6bbe8c103db35e52bc5e18a0bbde29ac8ac
cbstodd <cbstodd@gmail.com>
2019-03-11 16:39:47 -0400

    Updated footer items

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+    myIbdLifeLogo: string;
 
-  constructor() { }
+    constructor() {
+        this.myIbdLifeLogo = `https://firebasestorage.googleapis.com/v0/b/my-ibd-life-dev.appspot.com/o/images%2F2019%2F1552335053957_aga_logo_gi_patient_center_single_rev_cmyk.png?alt=media&token=54edfae9-8618-455e-9734-d5a166c58da0`;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
